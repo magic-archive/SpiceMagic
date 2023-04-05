f83a2069fd485fcf735575ee90745b946521d1e3
KesarThakkar <128405210+KesarThakkar@users.noreply.github.com>
2023-04-05 08:18:15 -0700

    Add files via upload
    Support read recipe from file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const article = document.querySelector("#content");
+/*const article = document.querySelector("#content");
 const btn = document.querySelector("#show-More")
 
 btn.addEventListener("click", showMore);
@@ -14,3 +14,61 @@ function showMore() {
     btn.innerHTML = "Show Less";
   }
 }
+*/
+
+function myFunction() {
+  var dropdown = document.getElementById("myDropdown");
+  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+  if (selectedOption == 'Khaman')
+  {
+   //alert("You selected " + selectedOption);
+   loadFile('file:\\\C:\\Documents\\Projects\\SpiceMagic\\dataImages\\Khaman.txt')
+  }
+   if (selectedOption == 'Paneer')
+   {
+   //alert("You selected " + selectedOption);
+   loadFile('Paneer.txt')
+   }
+   if (selectedOption == 'Gulab Jamun')
+   {
+   //alert("You selected " + selectedOption);
+   loadFile('..\\dataImages\\GulabJamun.txt')
+}
+}
+/*
+function loadFile(filename) {
+var xhr = new XMLHttpRequest();
+	xhr.open('GET', filename, true);
+	xhr.onreadystatechange = function() {
+    alert(xhr.readyState);
+    alert(xhr.status);
+		if (xhr.readyState == 4 && xhr.status == 200) {
+      alert(filename);
+			document.getElementById('text-container').value = xhr.responseText;
+		}
+	};
+	xhr.send();
+}
+*/
+
+function loadFile(filename) {
+  //alert(filename);
+ filename = "aboutUs.html";
+  //document.getElementById('text-container').value = "foobar ...";
+
+  fetch(filename)
+  .then(response => response.text())
+  .then(text => {
+    alert(response.text());
+    document.getElementById('text-container').value = text;
+  })
+  .catch(error => {
+    console.error('Error loading file:', error);
+  });
+
+}
+
+
+
+
+
