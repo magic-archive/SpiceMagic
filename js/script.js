b9274a8799ec865fce0bf2e9228dedb590b70de4
KesarThakkar <128405210+KesarThakkar@users.noreply.github.com>
2023-04-10 13:50:28 -0700

    Update script.js
    removed case statement

--- js/script.js
+++ js/script.js
@@ -20,67 +20,11 @@ function myFunction() {
   var filePath;
   var dropdown = document.getElementById("myDropdown");
   var selectedOption = dropdown.options[dropdown.selectedIndex].value;
-  if (selectedOption == 'Khaman')
-  {
-    filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
-    //alert("You selected " + filePath);
-    //loadFile(filePath + 'Recipe.txt');
-    //loadImage(filePath + 'Image.jpg')
-    loadFile('..\\dataImages\\Khaman\\Recipe.txt')
-  }
-   if (selectedOption == 'Samosas')
-   {
-   //alert("You selected " + selectedOption);
-   //loadFile('..\\dataImages\\Samosas.docx')
-     loadFile('..\\dataImages\\Kheer.txt')
-   }
-   if (selectedOption == 'Undhiyu')
-   {
-   //alert("You selected " + selectedOption);
-   //loadFile('..\\dataImages\\Undhiyu.docx')
-   loadFile('..\\dataImages\\Kheer.txt')
+  var filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
+  loadFile(filePath + 'Recipe.txt');
+  //loadImage(filePath + 'Image.jpg')
 }
-if (selectedOption == 'Dahi Tikhari')
-   {
-   //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Dahi Tikhari.txt')
-}
-if (selectedOption == 'Masala Khichdi')
-   {
-   //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Masala Khichdi.txt')
-}
-if (selectedOption == 'Thepla')
-   {
-   //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Thepla.txt')
-}
-if (selectedOption == 'Jalebi')
-   {
-   //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Jalebi.txt')
-}
-if (selectedOption == 'Kheer')
-   {
-   //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Kheer.txt')
-}
-}
-/*
-function loadFile(filename) {
-var xhr = new XMLHttpRequest();
-	xhr.open('GET', filename, true);
-	xhr.onreadystatechange = function() {
-    alert(xhr.readyState);
-    alert(xhr.status);
-		if (xhr.readyState == 4 && xhr.status == 200) {
-      alert(filename);
-			document.getElementById('text-container').value = xhr.responseText;
-		}
-	};
-	xhr.send();
-}
-*/
+
 
 function loadFile(filename) {
   alert(filename);
@@ -97,10 +41,7 @@ function loadFile(filename) {
 }
 function loadImage(imageName) {
   alert(imageName);
-
-    document.getElementById('image-container').value = imageName;
-
-
+  document.getElementById('image-container').value = imageName;
 }
 
 
