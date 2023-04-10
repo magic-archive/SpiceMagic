92552c09500fd7f03dc4261e058a6988d2e85165
KesarThakkar <128405210+KesarThakkar@users.noreply.github.com>
2023-04-10 15:40:50 -0700

    Update script.js
    Cleaned up unwanted code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-
-
+function myFunction() {
+  var dropdown = document.getElementById("myDropdown");
+  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+  var filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
+  loadHTML(filePath + 'Recipe.html')
+  //loadFile(filePath + 'Recipe.txt');
+}
+function loadHTML(filename) {
+  //alert(filename);
+  fetch(filename)
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('recipe-container').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Error loading file:', error);
+  });
+}
 function loadFile(filename) {
   alert(filename);
   fetch(filename)
@@ -12,47 +28,6 @@ function loadFile(filename) {
   });
 }
 
-function myFunction() {
-  var dropdown = document.getElementById("myDropdown");
-  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
-  var filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
-  loadHTML(filePath + 'Recipe.html')
-  //loadFile(filePath + 'Recipe.txt');
-  //loadImage(filePath + 'Image.jpg')
-}
-function loadHTML(filename) {
-  alert(filename);
-  fetch(filename)
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('recipe-container').innerHTML = data;
-  })
-  .catch(error => {
-    console.error('Error loading file:', error);
-  });
-}
-function loadImage(imageName) {
-  //alert(imageName);
-    document.getElementById('image-container').value = imageName;
-}
-function myRecipe() {
-  var dropdown = document.getElementById("myDropdown");
-  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
-  var filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
-  loadFile(filePath + 'Recipe.txt');
-}
-function myMethod() {
-  var dropdown = document.getElementById("myDropdown");
-  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
-  var filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
-  loadFile(filePath + 'Method.txt');
-}
-function myIngredients() {
-  var dropdown = document.getElementById("myDropdown");
-  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
-  var filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
-  loadFile(filePath + 'Ingredients.txt');
-}
 
 
 
