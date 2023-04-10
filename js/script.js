d407e7942b2def1fb1465888b85cfab90616f6ad
KesarThakkar <128405210+KesarThakkar@users.noreply.github.com>
2023-04-10 13:15:56 -0700

    Add files via upload
    Added support for a subfolder

--- js/script.js
+++ js/script.js
@@ -17,63 +17,93 @@ function showMore() {
 */
 
 function myFunction() {
+  var filePath;
   var dropdown = document.getElementById("myDropdown");
   var selectedOption = dropdown.options[dropdown.selectedIndex].value;
   if (selectedOption == 'Khaman')
   {
-   //alert("You selected " + selectedOption);
-   //loadFile('..\\dataImages\\Khaman.docx')
-    loadFile('..\\dataImages\\Kheer.txt')
+    filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
+    alert("You selected " + filePath);
+    loadFile(filePath + 'Recipe.txt');
+    loadImage(filePath + 'Image.jpg')
+    //loadFile('..\\dataImages\\Kheer.txt')
   }
    if (selectedOption == 'Samosas')
    {
    //alert("You selected " + selectedOption);
    //loadFile('..\\dataImages\\Samosas.docx')
-     loadFile('..\\dataImages\\Kheer.rtf')
+     loadFile('..\\dataImages\\Kheer.txt')
    }
    if (selectedOption == 'Undhiyu')
    {
    //alert("You selected " + selectedOption);
    //loadFile('..\\dataImages\\Undhiyu.docx')
-   loadFile('..\\dataImages\\Kheer.htm')
+   loadFile('..\\dataImages\\Kheer.txt')
 }
 if (selectedOption == 'Dahi Tikhari')
    {
    //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Dahi Tikhari.docx')
+   loadFile('..\\dataImages\\Dahi Tikhari.txt')
 }
 if (selectedOption == 'Masala Khichdi')
    {
    //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Masala Khichdi.docx')
+   loadFile('..\\dataImages\\Masala Khichdi.txt')
 }
 if (selectedOption == 'Thepla')
    {
    //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Thepla.docx')
+   loadFile('..\\dataImages\\Thepla.txt')
 }
 if (selectedOption == 'Jalebi')
    {
    //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Jalebi.docx')
+   loadFile('..\\dataImages\\Jalebi.txt')
 }
 if (selectedOption == 'Kheer')
    {
    //alert("You selected " + selectedOption);
-   loadFile('..\\dataImages\\Kheer.docx')
+   loadFile('..\\dataImages\\Kheer.txt')
 }
-
 }
+/*
 function loadFile(filename) {
-  //alert(filename);
-  
+var xhr = new XMLHttpRequest();
+	xhr.open('GET', filename, true);
+	xhr.onreadystatechange = function() {
+    alert(xhr.readyState);
+    alert(xhr.status);
+		if (xhr.readyState == 4 && xhr.status == 200) {
+      alert(filename);
+			document.getElementById('text-container').value = xhr.responseText;
+		}
+	};
+	xhr.send();
+}
+*/
+
+function loadFile(filename) {
+  alert(filename);
   fetch(filename)
   .then(response => response.text())
   .then(text => {
-     document.getElementById('text-container').value = text;
+    alert(response.text());
+    document.getElementById('text-container').value = text;
   })
   .catch(error => {
     console.error('Error loading file:', error);
   });
 
 }
+function loadImage(imageName) {
+  alert(imageName);
+
+    document.getElementById('image-container').value = imageName;
+
+
+}
+
+
+
+
+
