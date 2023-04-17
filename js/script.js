7dea882cba0df22a51c13904829f53df44bb5442
KesarThakkar <128405210+KesarThakkar@users.noreply.github.com>
2023-04-17 22:38:19 +0530

    Update script.js
    not needed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function myFunction() {
   var selectedOption = dropdown.options[dropdown.selectedIndex].value;
   var filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
   loadHTML(filePath + 'recipe.htm')
-  //loadFile(filePath + 'Recipe.txt');
+ 
 }
 
 function loadHTML(filename) {
@@ -12,17 +12,6 @@ function loadHTML(filename) {
   .then(response => response.text())
   .then(data => {
     document.getElementById('recipe-container').innerHTML = data;
-  })
-  .catch(error => {
-    console.error('Error loading file:', error);
-  });
-}
-function loadFile(filename) {
-  alert(filename);
-  fetch(filename)
-  .then(response => response.text())
-  .then(text => {
-    document.getElementById('text-container').value = text;
   })
   .catch(error => {
     console.error('Error loading file:', error);
