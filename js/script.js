aa07996fd3eab4cac5574192444e2bd623633832
KesarThakkar <128405210+KesarThakkar@users.noreply.github.com>
2023-04-19 13:42:27 +0530

    Update script.js
    Added hindi and gujarati recipes button

--- js/script.js
+++ js/script.js
@@ -17,6 +17,27 @@ function loadHTML(filename) {
     console.error('Error loading file:', error);
   });
 }
+function myFunction1() {
+  var dropdown = document.getElementById("myDropdown");
+  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+
+  var selectedLang; 
+  let engL =  document.getElementById("radio1");
+  let hinL = document.getElementById("radio2");
+  let gujL = document.getElementById("radio3");
+  if (engL.checked)
+   selectedLang = engL.value;
+   if (hinL.checked)
+   selectedLang = hinL.value;
+   if (gujL.checked)
+   selectedLang = gujL.value;
+
+var dropdown = document.getElementById("myDropdown");
+  var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+  var filePath = '..\\dataImages' + '\\' + selectedOption + '\\' + selectedLang;
+  loadHTML(filePath + selectedLang)
+}
+
 
 
 
