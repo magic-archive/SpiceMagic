c83c739a486e323ce51ac111833b46b184aa0a7b
KesarThakkar <128405210+KesarThakkar@users.noreply.github.com>
2023-04-19 21:18:25 +0530

    Update script.js
    updated the file path

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ function myFunction() {
   var dropdown = document.getElementById("myDropdown");
   var selectedOption = dropdown.options[dropdown.selectedIndex].value;
   var filePath = '..\\dataImages' + '\\' + selectedOption + '\\';
-  loadHTML(filePath + 'recipe.htm')
+ // loadHTML(filePath + 'recipe.htm')
+  loadHTML('..\\dataImages\\Dahi Tikhari\\DahiTikhar.htm');
  
 }
 
